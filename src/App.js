590ba36393de6ccb4baf11ed6f4a6a96a9fba8ac
Vimal Vs <vimalchan23@gmail.com>
2023-08-04 22:47:02 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import ItemList from "./components/ItemList";
 import AddItemForm from "./components/AddItemForm";
 import EditItemForm from "./components/EditItemForm";
+import Header from "./components/widgets/Header";
+import Footer from "./components/widgets/Footer";
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-5">
-      <ItemList />
-      <AddItemForm />
-      <EditItemForm />
+    <div className="flex flex-col justify-between min-h-full md:min-h-fit md:h-screen">
+      <Header />
+      <div className="flex flex-col justify-between h-full">
+        <ItemList />
+        <div className="flex flex-col gap-6 my-8 md:gap-0 md:flex-row justify-evenly items-center md:my-auto">
+          <AddItemForm />
+          <EditItemForm />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/EditItemForm.jsx
+++ src/components/EditItemForm.jsx
@@ -1,33 +1,46 @@
 import axios from "axios";
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function EditItemForm() {
-  const inputFieldStyle = "border-2 rounded-md";
-  const labelStyle = "";
+
+  const inputFieldStyle = "border-2 rounded-md px-1";
+  const labelStyle = "text-gray-600";
 
   const handleSubmit = (event) => {
+
     event.preventDefault();
     const name = event.target.name.value;
     const updateName = event.target.edit_name.value;
-    axios.patch(`http://localhost:3001/data/edit/${name}`, {updateName})
+    const updateDescription = event.target.description.value;
+    axios.patch(`${process.env.REACT_APP_API_URL}/data/edit/${name}`, { updateName, updateDescription })
     .then(res => {
-        console.log(res);
+      if(res.data === null)toast.error("Name not found !");
+      if(res.data)toast.success("Edited Successfully !");
     })
     .catch(err => {
-        console.log(err);
+        toast.error("New Name already exists !");
     })
-  }
+  };
+
   return (
-    <form className="flex flex-col gap-4 justify-center items-center pb-6" onSubmit={handleSubmit}>
-      <h2 className="text-xl">Edit Item</h2>
+    <form onSubmit={ handleSubmit }>
+      <div className="flex flex-col gap-6 py-8 justify-center items-center rounded-md shadow-lg border border-slate-300/50 px-4">
+        <h2 className="text-xl">Edit Item</h2>
         <div className="flex gap-2">
-            <label htmlFor="name" className={labelStyle}>Search : </label>
+            <label htmlFor="name" className={labelStyle}>Old Name : </label>
             <input type="text" name="name" className={inputFieldStyle} required/>
         </div>
         <div className="flex gap-2">
-            <label htmlFor="edit_name" className={labelStyle}>Edit with : </label>
+            <label htmlFor="edit_name" className={labelStyle}>New Name : </label>
             <input type="text" name="edit_name" className={inputFieldStyle} required/>
         </div>
-      <button type="submit" className="w-fit rounded-lg px-2 py-1 border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-white transition duration-200">Edit Item</button>
+        <div className="flex gap-2">
+            <label htmlFor="description" className={labelStyle}>Description : </label>
+            <input type="text" name="description" className={inputFieldStyle} required/>
+        </div>
+        <button type="submit" className="w-fit rounded-lg px-2 py-1 border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-200">Edit Data</button>
+      </div>
+      <Toaster />
     </form>
   )
-}
+};
