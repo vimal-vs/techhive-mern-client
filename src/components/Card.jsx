1f576022eeb31147d78c908e67b35476d3b9abbd
Vimal Vs <vimalchan23@gmail.com>
2023-08-04 22:43:12 +0530

    added widgets

--- src/components/Card.jsx
+++
@@ -1,8 +0,0 @@
-export default function Card( {name, desc} ) {
-  return (
-    <ul key={name} className='flex flex-col shadow-lg shadow-slate-200 px-4 py-2 rounded-lg w-fit'>
-        <li>Name : {name}</li> 
-        <li>Description : {desc}</li> 
-    </ul>
-  )
-}
---
+++ src/components/widgets/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <nav className="flex justify-center items-center bg-slate-700 h-[6vh]">
+      <h1 className="text-white md:text-xl py-2">MERN App - CRUD Operations</h1>
+    </nav>
+  )
+}
