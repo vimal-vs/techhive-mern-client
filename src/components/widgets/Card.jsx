94dab06dab17fa6e39a88699189623d3f4eb2a55
Vimal Vs <vimalchan23@gmail.com>
2023-08-05 11:51:52 +0530

    added word break

--- src/components/widgets/Card.jsx
+++ src/components/widgets/Card.jsx
@@ -1,8 +1,8 @@
 export default function Card({ name, desc }) {
   return (
     <ul className='flex flex-col shadow-lg shadow-slate-200 px-4 py-2 rounded-lg border border-slate-300/50 w-fit'>
-        <li><span className="text-gray-500">Name :</span> { name }</li> 
-        <li><span className="text-gray-500">Description :</span> { desc }</li> 
+        <li className="break-all"><span className="text-gray-500">Name :</span> { name }</li> 
+        <li className="break-all"><span className="text-gray-500">Description :</span> { desc }</li> 
     </ul>
   )
 }
